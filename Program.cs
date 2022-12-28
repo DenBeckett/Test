5b34e8121c02e23fcac70f13c15a1d6d43c9cb3f
Den Beckett <den_local@debian>
2022-12-28 20:56:39 +0300

    write program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string[] array = {"hello","2","world",":)"};
+
+//Console.WriteLine("Введите строки через пробел");
+//string rows = Console.ReadLine();
+//array = rows.Split(" ");
+
+string[] resultArray = new string[array.Length];
+int size = 0;
+foreach (string row in array)
+{
+    if (row.Length <= 3)
+    {
+        resultArray[size] = row;
+        size += 1;
+    }
+}
+
+PrintArray(resultArray);
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write($"{array[index]} ");
+        index++;
+    }
+    Console.WriteLine();
+}
